d256d8252f91314c714e3b9011df5ed606211b42
KANSEI CHIN <qq1528359634@gmail.com>
2026-02-01 11:54:50 +0900

    depart some methods
    depart some methods make it cleaner

--- FolderMovingAssistant/FolderMovingAssistant/MainWindow.xaml.cs
+++ FolderMovingAssistant/FolderMovingAssistant/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -16,13 +18,13 @@ namespace FolderMovingAssistant
             InitializeComponent();
         }
 
-
+        #region Set source Path and destination Path
         private void SelectMonPath_Click(object sender, RoutedEventArgs e)
         {
-            MyObejects.MonPath = MyMethods.GetPath(monPathBox);
-            if (!string.IsNullOrWhiteSpace(MyObejects.MonPath))
+            FolderPath.destiPath = MyMethods.GetPath(monPathBox);
+            if (!string.IsNullOrWhiteSpace(FolderPath.destiPath))
             {
-                MyObejects.MonFolders = MyMethods.GetFolders(MyObejects.MonPath);
+                FolderPath.destiFolders = MyMethods.GetFoldersLIst(FolderPath.destiPath);
 
             }
 
@@ -30,14 +32,15 @@ namespace FolderMovingAssistant
         }
         private void SelectSonPath_Click(object sender, RoutedEventArgs e)
         {
-            MyObejects.SonPath = MyMethods.GetPath(sonPathBox);
+            FolderPath.sourcePath = MyMethods.GetPath(sonPathBox);
 
-            if (!string.IsNullOrWhiteSpace(MyObejects.SonPath))
+            if (!string.IsNullOrWhiteSpace(FolderPath.sourcePath))
             {
-                MyObejects.SonFolders = MyMethods.GetFolders(MyObejects.SonPath);
-                var rawList = MyObejects.SonFolders;
+                FolderPath.sourceFolders = MyMethods.GetFoldersLIst(FolderPath.sourcePath);
+                var rawList = FolderPath.sourceFolders;
                 // 2. 加工数据：将 string 转换为包含 Id 和 Text 的匿名对象
                 // index + 1 是为了让序号从 1 开始，而不是 0
+                //show source folders list in ListView
                 var itemsWithIndex = rawList.Select((e, Index) => new
                 {
                     Id = Index + 1,
@@ -50,93 +53,147 @@ namespace FolderMovingAssistant
 
             }
         }
+        #endregion
 
+        #region floders move method
         private void Move_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(MyObejects.SonPath) || string.IsNullOrWhiteSpace(MyObejects.MonPath))
+            if (string.IsNullOrWhiteSpace(FolderPath.sourcePath) || string.IsNullOrWhiteSpace(FolderPath.destiPath))
             {
                 MessageBox.Show("Please configure the complete folder path first!");
                 return;
             }
             else
             {
-                var list = MyMethods.MoveFolder();
-                var moveList = list.Where(e => e.Key.Count() == 1).Select(e => new
-                {
-                    Key = e.Key.First(),
-                    Value = e.Value
-                }).ToList();
-                foreach (var item in moveList)
-                {   //别忘记追加斜杆 
-                    //string destiPath = MyObejects.MonPath +"\\"+item.Key;
-                    //string sourcePath = MyObejects.SonPath +"\\"+item.Value;
-                    string soucePath = Path.Combine(MyObejects.SonPath, item.Value);
-                    string destiePath = Path.Combine(MyObejects.MonPath, Path.Combine(item.Key, item.Value));
-                    string sourceDisk = Path.GetPathRoot(soucePath);
-                    string destiDisk = Path.GetPathRoot(destiePath);
-                    if (sourceDisk==destiDisk)
-                    {
-                        try
-                        {
-                            Directory.Move(soucePath, destiePath);
-                        }
-                        catch (Exception excep)
-                        {
-                            MessageBox.Show(excep.Message);
-                        }
-                    }
-                    else
-                    {
-
-                    }
-                    
-
-                }
+                var relevantFolders = MyMethods.GetRelevantFolders();
+                var sortedList = MyMethods.SortOneRelevantFolders(relevantFolders);
+                MyMethods.ExecuteMoveFolder(sortedList);
                 MessageBox.Show("Move Finished！");
+
+            }
+
+        }
+
+    }
+    #endregion
+}
+class FolderPath
+{
+    public static string? destiPath;
+    public static string? sourcePath;
+
+    public static List<string>? destiFolders;
+    public static List<string>? sourceFolders;
+}
+class SortedFolder
+{
+    public string? destiFolder { get; set; }
+    public string? SourceFolder { get; set; }
+}
+class MyMethods
+{    ///<summary>
+     ///select disk folder path
+     ///</summary>
+    internal static string GetPath(TextBox box)
+    {
+        string path = "";
+        //判断是否选择路径，如果选择路径则执行 if内语句
+        OpenFolderDialog openFolderDialog = new OpenFolderDialog();
+        if (openFolderDialog.ShowDialog() == true)
+        {
+            path = openFolderDialog.FolderName;
+            box.Text = path;
+        }
+        //返回选择路径
+        return path;
+    }
+    /// <summary>
+    /// get source folders list adn destination folders list
+    /// </summary>
+    internal static List<string> GetFoldersLIst(string path)
+    {                         //获取目录下的所有子文件夹路径 并只保留文件夹名 转换为list集合
+        List<string> foldersList = Directory.EnumerateDirectories(path).Select(e => Path.GetFileName(e)).ToList();
+        return foldersList;
+    }
+
+    internal static Dictionary<IEnumerable<string>, string> GetRelevantFolders()
+    {
+        //返回一个合集 key为匹配到的母文件夹(key is IEnumerabkle) value为匹配到的子文件夹
+        var relevantFolders = FolderPath.sourceFolders.Select(son => new
+        {
+            Values = son,
+            key = FolderPath.destiFolders.Where(e => son.Contains(e))
+        }
+        ).ToDictionary(x => x.key, y => y.Values);
+        return relevantFolders;
+    }
+    internal static List<SortedFolder> SortOneRelevantFolders(Dictionary<IEnumerable<string>, string> relevantFolders)
+    {
+        var sortedList = relevantFolders.Where(pairs => pairs.Key.Count() == 1).Select(onePairs => new SortedFolder
+        {
+            destiFolder = onePairs.Key.First(),
+            SourceFolder = onePairs.Value
+        }).ToList();
+        return sortedList;
+    }
+    internal static void ExecuteMoveFolder(List<SortedFolder> sortedList)
+    {
+        foreach (var item in sortedList)
+        {
+            string soucePath = Path.Combine(FolderPath.sourcePath, item.SourceFolder);
+            string destiePath = Path.Combine(FolderPath.destiPath, Path.Combine(item.destiFolder, item.SourceFolder));
+            string sourceDisk = Path.GetPathRoot(soucePath);
+            string destiDisk = Path.GetPathRoot(destiePath);
+            if (!Path.Exists(destiePath)&&Path.Exists(soucePath))
+            {
+                if (sourceDisk == destiDisk)
+                {
+                    try
+                    {
+                        Directory.Move(soucePath, destiePath);
+                    }
+                    catch (Exception excep)
+                    {
+                        MessageBox.Show(excep.Message);
+                    }
+                }
+                else
+                {
+                    MoveFoldersAcrossDisk(soucePath, destiePath);
+                }
+            }
+            else
+            {
+                MessageBox.Show($" destination folder {item.destiFolder}  already existed! or\n" +
+                    $"source folder{item.SourceFolder} is missed!");
             }
 
         }
     }
-    class MyObejects
-    {
-        public static string? MonPath;
-        public static string? SonPath;
 
-        public static List<string>? MonFolders;
-        public static List<string>? SonFolders;
-    }
-    class MyMethods
+    internal static void MoveFoldersAcrossDisk(string soucePath, string destiePath)
     {
-        internal static string GetPath(TextBox box)
-        {
-            string path = "";
-            //判断是否选择路径，如果选择路径则执行 if内语句
-            OpenFolderDialog openFolderDialog = new OpenFolderDialog();
-            if (openFolderDialog.ShowDialog() == true)
+        try
+        {   
+            DirectoryInfo dir = new DirectoryInfo(soucePath);
+            Directory.CreateDirectory(destiePath);
+            foreach (FileInfo file in dir.GetFiles())
             {
-                path = openFolderDialog.FolderName;
-                box.Text = path;
+                string filePath = Path.Combine(destiePath, file.Name);
+                file.CopyTo(filePath, true);
             }
-            //返回选择路径
-            return path;
-        }
-        internal static List<string> GetFolders(string path)
-        {                         //获取目录下的所有子文件夹路径 并只保留文件夹名 转换为list集合
-            List<string> Folders = Directory.EnumerateDirectories(path).Select(e => Path.GetFileName(e)).ToList();
-            return Folders;
-        }
-
-        internal static Dictionary<IEnumerable<string>, string> MoveFolder()
-        {
-            //返回一个合集 key为匹配到的母文件夹 value为匹配到的子文件夹
-            var associatedFolder = MyObejects.SonFolders.Select(son => new
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
             {
-                Values = son,
-                key = MyObejects.MonFolders.Where(e => son.Contains(e))
+                string newDestiePath = Path.Combine(destiePath, subDir.Name);
+                MoveFoldersAcrossDisk(subDir.FullName, newDestiePath);
             }
-            ).ToDictionary(x => x.key, y => y.Values);
-            return associatedFolder;
+            dir.Delete(true);
         }
+        catch (Exception excep)
+        {
+            MessageBox.Show(excep.Message);
+        }
+       
 
     }
 }
