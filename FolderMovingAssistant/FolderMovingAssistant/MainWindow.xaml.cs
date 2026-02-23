ff10a85f8be0ab1819e2caaa0ee8efb42ebc57dc
KANSEI CHIN <qq1528359634@gmail.com>
2026-02-23 12:36:54 +0900

    add put ExceptionFilter

--- FolderMovingAssistant/FolderMovingAssistant/MainWindow.xaml.cs
+++ FolderMovingAssistant/FolderMovingAssistant/MainWindow.xaml.cs
@@ -23,36 +23,38 @@ namespace FolderMovingAssistant
         #region Set source Path and destination Path
         private void SelectMonPath_Click(object sender, RoutedEventArgs e)
         {
-            FolderPath.destiPath = MyMethods.GetPath(monPathBox);
+            FolderPath.destiPath = MyMethods.GetPath(targerPathBox);
             if (!string.IsNullOrWhiteSpace(FolderPath.destiPath))
-            {
+            {   //若路径不为空 则读取路径下所有文件夹
                 FolderPath.destiFolders = MyMethods.GetFoldersLIst(FolderPath.destiPath);
-
             }
 
 
         }
         private void SelectSonPath_Click(object sender, RoutedEventArgs e)
-        {
-            FolderPath.sourcePath = MyMethods.GetPath(sonPathBox);
-
-            if (!string.IsNullOrWhiteSpace(FolderPath.sourcePath))
-            {
+        {   
+            FolderPath.sourcePath = MyMethods.GetPath(sourcePathBox);
+            if (!string.IsNullOrWhiteSpace(FolderPath.sourcePath) && FolderPath.sourcePath!= FolderPath.destiPath)
+            { //若路径不为空 则读取路径下所有文件夹
                 FolderPath.sourceFolders = MyMethods.GetFoldersLIst(FolderPath.sourcePath);
-                var rawList = FolderPath.sourceFolders;
+                var sourceList = FolderPath.sourceFolders;
                 // 2. 加工数据：将 string 转换为包含 Id 和 Text 的匿名对象
                 // index + 1 是为了让序号从 1 开始，而不是 0
                 //show source folders list in ListView
-                var itemsWithIndex = rawList.Select((e, Index) => new
+                var itemsWithIndex = sourceList.Select((e, Index) => new
                 {
                     Id = Index + 1,
                     Text = e
                 }).ToList();
 
                 // 3. 赋值给控件
-                sonFolderList.ItemsSource = itemsWithIndex;
+                sourceFolderList.ItemsSource = itemsWithIndex;
                 return;
-
+            }
+            else
+            {   
+                MessageBox.Show("the sourcePath is null or the same to targerPath!,Please chose againt!");
+                sourcePathBox.Text = "";
             }
         }
         #endregion
